8ed4b4034c45f8428403ea55404de9520549d304
frodriguez9 <frodriguez.dagostino@gmail.com>
2017-02-21 20:14:02 -0600

    Initial commit

--- app/src/main/java/com/raffle/androidrafflegame/LoginActivity.java
+++ app/src/main/java/com/raffle/androidrafflegame/LoginActivity.java
@@ -3,9 +3,11 @@ package com.raffle.androidrafflegame;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
@@ -20,6 +22,8 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
@@ -28,6 +32,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,47 +42,52 @@ import static android.Manifest.permission.READ_CONTACTS;
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+                public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
+                    /* Made by Holly Kwan.  */
+                    private TextView actionBar;
+                    /**
+                     * Id to identity READ_CONTACTS permission request.
+                     */
+                    private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    private UserLoginTask mAuthTask = null;
+                    /**
+                     * A dummy authentication store containing known user names and passwords.
+                     * TODO: remove after connecting to a real authentication system.
+                     */
+                    private static final String[] DUMMY_CREDENTIALS = new String[]{
+                            "foo@example.com:hello", "bar@example.com:world"
+                    };
+                    /**
+                     * Keep track of the login task to ensure we can cancel it if requested.
+                     */
+                    private UserLoginTask mAuthTask = null;
 
-    // UI references.
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
-    private View mProgressView;
-    private View mLoginFormView;
+                    // UI references.
+                    private AutoCompleteTextView mEmailView;
+                    private EditText mPasswordView;
+                    private View mProgressView;
+                    private View mLoginFormView;
+                    private TextView mTextView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
+                    //actionBar.getSupportActionBar().setDisplayOptions(ActionBar.DisplayOptions());
 
-        mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
-                    return true;
-                }
+                    @Override
+                    protected void onCreate(Bundle savedInstanceState) {
+                        super.onCreate(savedInstanceState);
+                        setContentView(R.layout.activity_login);
+                        // Set up the login form.
+                        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+                        populateAutoComplete();
+
+                        mPasswordView = (EditText) findViewById(R.id.password);
+                        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                            @Override
+                            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                                    attemptLogin();
+                                    return true;
+                                }
                 return false;
             }
         });
@@ -92,8 +102,18 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
-    }
 
+        /* Holly Kwan implemented and called methods below. (@CopyRight Holly Kwan) */
+        TextView textView = (TextView)findViewById(R.id.signup);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+        }
     private void populateAutoComplete() {
         if (!mayRequestContacts()) {
             return;
